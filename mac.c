84f67e36a2b616a6fb9feb425d9858874246dc23
QianXin <qxlfruitsalad@gmail.com>
2017-06-12 17:24:00 +0800

    Create mac.c

--- mac.c
+++ mac.c
@@ -1,29 +1,283 @@
-#include   <stdio.h>
-#include   <sys/ioctl.h>
-#include   <sys/socket.h>
-#include   <netinet/in.h>
-#include   <net/if.h>
-#include   <string.h>
-char *get_local_mac(char *if_name){
-    struct ifreq m_ifreq;
-    int sock = 0;
-    char mac[32] = " ";
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <net/if.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <linux/if_packet.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+#define FRAME_LEN 14
+#define MAX_BUF_LEN 1024  
+#define MAX_KEY_LEN 64  
+#define MAX_VAL_LEN 256  
+#define chartonumber(x)(x-'0')
+typedef unsigned int u_int32;
+typedef unsigned short u_int16;
+typedef unsigned char u_int8;
 
-    sock = socket(AF_INET,SOCK_STREAM,0);
-    strcpy(m_ifreq.ifr_name,if_name);
+typedef struct FrameHeader_t
+{
+u_int8 DstMAC[6];
+u_int8 SrcMAC[6];
+u_int16 FrameType;
+} FrameHeader_t;
 
-    ioctl(sock,SIOCGIFHWADDR,&m_ifreq);
+typedef struct IPHeader_t
+{
+u_int8 Ver_IHLen;
+u_int8 TOS;
+u_int16 TotalLen;
+u_int16 ID;
+u_int16 Flag_Segment;
+u_int8 TTL;
+u_int8 Protocol;
+u_int16 Checksum;
+u_int32 SrcIP;
+u_int32 DstIP;
+} IPHeader_t;
+
+typedef struct UDPHeader_t
+{
+u_int16 SrcPort;
+u_int16 DstPort;
+u_int16 Len;
+u_int16 Checksum;
+} UDPHeader_t;
+
+typedef struct Configuration_t
+{
+char* Iface;
+char* Olddest;
+} Configuration_t;
+
+int Trim(char s[])  
+{  
+    int n;  
+    for(n = strlen(s) - 1; n >= 0; n--)  
+    {  
+        if(s[n]!=' ' && s[n]!='\t' && s[n]!='\n')  
+            break;  
+        s[n+1] = '\0';  
+    }  
+    return n;  
+}  
+  
+int parseInt(char str[], int length)
+{
+    int number=0;
     int i = 0;
-    for(i = 0; i < 6; i++){
-        sprintf(mac+3*i, "%02X:", (unsigned char) m_ifreq.ifr_hwaddr.sa_data[i]);
+    for(i=0;i<(length-1);i++)
+    {
+        int position=1;
+        int a = 0;
+        for(a=0;a<i;a++)
+        {
+            position*=10;
+        }
+        number+=chartonumber(str[i])*position;
     }
-    mac[strlen(mac) - 1] = 0;
-    printf("%s\n", mac);
+    return number;
+}
 
-    return NULL;
+Configuration_t* loadConfigDemo(const char* config_path)  
+{  
+    FILE * file = fopen(config_path, "r");  
+    if (file == NULL)  
+    {  
+        printf("[Error]open %s failed.\n", config_path);  
+        return NULL;  
+    }  
+    char buf[MAX_BUF_LEN];  
+    int text_comment = 0;  
+    char* olddest;
+    char* iface;
+    while(fgets(buf, MAX_BUF_LEN, file) != NULL)  
+    {  
+        Trim(buf);  
+        // to skip text comment with flags /* ... */  
+        if (buf[0] != '#' && (buf[0] != '/' || buf[1] != '/'))  
+        {  
+            if (strstr(buf, "/*") != NULL)  
+            {  
+                text_comment = 1;  
+                continue;  
+            }  
+            else if (strstr(buf, "*/") != NULL)  
+            {  
+                text_comment = 0;  
+                continue;  
+            }  
+        }  
+        if (text_comment == 1)  
+        {  
+            continue;  
+        }  
+        int buf_len = strlen(buf);  
+        // ignore and skip the line with first chracter '#', '=' or '/'  
+        if (buf_len <= 1 || buf[0] == '#' || buf[0] == '=' || buf[0] == '/')  
+        {  
+            continue;  
+        }  
+        buf[buf_len-1] = '\0';  
+        char _paramk[MAX_KEY_LEN] = {0}, _paramv[MAX_VAL_LEN] = {0};  
+        int _kv=0, _klen=0, _vlen=0;  
+        int i = 0;
+        for (i=0; i<buf_len; ++i)  
+        {  
+            if (buf[i] == ' ')  
+                continue;  
+            // scan param key name  
+            if (_kv == 0 && buf[i] != '=')  
+            {  
+                if (_klen >= MAX_KEY_LEN)  
+                    break;  
+                _paramk[_klen++] = buf[i];  
+                continue;  
+            }  
+            else if (buf[i] == '=')  
+            {  
+                _kv = 1;  
+                continue;  
+            }  
+            // scan param key value  
+            if (_vlen >= MAX_VAL_LEN || buf[i] == '#')  
+                break;  
+            _paramv[_vlen++] = buf[i];  
+        }  
+        if (strcmp(_paramk, "")==0 || strcmp(_paramv, "")==0)  
+            continue;  
+        
+        if (strcmp(_paramk, "ports")==0)
+        {
+            int port=parseInt(_paramv, _vlen);
+        }
+        if (strcmp(_paramk, "newport")==0)
+        {
+            int newport=parseInt(_paramv, _vlen);
+        }
+        if (strcmp(_paramk, "olddest")==0)
+        {
+            olddest=_paramv;
+        }
+        if (strcmp(_paramk, "newsrc")==0)
+        {
+            char* newsrc;
+            newsrc=_paramv;
+        }
+        if (strcmp(_paramk, "newdest")==0)
+        {
+            char* newdest;
+            newdest=_paramv;
+        }
+        if (strcmp(_paramk, "newsrcmac")==0)
+        {
+            char* newsrcmac;
+            newsrcmac=_paramv;
+        }
+        if (strcmp(_paramk, "newdstmac")==0)
+        {
+            char* newdstmac;
+            newdstmac=_paramv;
+        }
+        if (strcmp(_paramk, "iface")==0)
+        {
+            iface=_paramv;
+        }
+    }  
+    struct Configuration_t conf ={iface,olddest};
+    return &conf;
+}
+
+int analyseFrame(FrameHeader_t *frame,u_int8 *mac)
+{
+    u_int8* p = (u_int8*)&frame->DstMAC;
+    p = (u_int8*)&frame->SrcMAC; 
+    int i=0;
+    for(i=0;i<6;i++)
+        if(mac[i]!=p[i]) return 1;
+    return 0;    
+}
+
+int analyseIP(IPHeader_t *ip,u_int8 *desIP)
+{
+    u_int8* p = (u_int8*)&ip->SrcIP;
+    //printf("Source IP\t: %u.%u.%u.%u\n",p[0],p[1],p[2],p[3]);
+    int i=0;
+    for(i=0;i<4;i++)
+        if(desIP[i]!=p[i]) return 1;
+    //p = (u_int8*)&ip->DstIP;
+    //printf("Destination IP\t: %u.%u.%u.%u\n",p[0],p[1],p[2],p[3]);
+    return 0;
+}
+
+void analyseUDP(UDPHeader_t *udp)
+{
+    printf("UDP -----\n");
+    printf("Source port: %u\n", ntohs(udp->SrcPort));
+    printf("Dest port: %u\n", ntohs(udp->DstPort));
 }
 
 int main(){
-    get_local_mac("enp0s3");
+    Configuration_t *conf;
+    conf = loadConfigDemo("./setting.conf");
+    struct ifreq m_ifreq;
+    int sock = 0;
+    sock = socket(AF_INET,SOCK_STREAM,0);
+    strcpy(m_ifreq.ifr_name,conf->Iface);
+    ioctl(sock,SIOCGIFHWADDR,&m_ifreq);
+    u_int8* mac=(u_int8*)m_ifreq.ifr_hwaddr.sa_data;
+    u_int8 *oldIP = strtok(conf->Olddest,".");
+    FrameHeader_t *frame;
+    char buf[12000];//1500 MTU
+    ssize_t n;
+    /* capture ip datagram without ethernet header */
+    if ((sock = socket(PF_PACKET,  SOCK_RAW, htons(ETH_P_ALL)))== -1)
+    {    
+        printf("socket error!\n");
+        return 1;
+    }
+    while (1)
+    {
+        n = recv(sock, buf, sizeof(buf), 0);
+        if (n == -1)
+        {
+            printf("recv error!\n");
+            break;
+        }
+        else if (n==0)
+            continue;
+        frame = (FrameHeader_t *)(buf);
+        if(analyseFrame(frame,mac)==1)
+            continue;
+        IPHeader_t *ip = ( IPHeader_t *)(buf + FRAME_LEN);
+        if(analyseIP(ip,oldIP)==1)
+            continue;
+        size_t iplen =  (ip->Ver_IHLen&0x0f)*4;
+        if (ip->Protocol == IPPROTO_UDP)
+        {
+            UDPHeader_t *udp = (UDPHeader_t *)(buf + FRAME_LEN + iplen);
+            analyseUDP(udp);
+        }
+        else if (ip->Protocol == IPPROTO_TCP)
+        {
+            printf("TCP----\n");
+        }
+        else if (ip->Protocol == IPPROTO_ICMP)
+        {
+            printf("ICMP----\n");
+        }
+        else if (ip->Protocol == IPPROTO_IGMP)
+        {
+            printf("IGMP----\n");
+        }
+        else
+        {
+            printf("other protocol!\n");
+        }        
+        printf("\n\n");
+    }
+    close(sock);
     return 0;
 }
